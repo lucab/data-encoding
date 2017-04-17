e140dfeeefe05b83ca5a57f76a3db4e373bc5b65
ia0 <git@ia0.eu>
2017-04-17 13:18:02 +0200

    v1.1.1

---
+++ v1.1.1/implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["data_encoding"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='trait core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='data_encoding/base/enum.ValidError.html' title='enum data_encoding::base::ValidError'>ValidError</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='trait core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='data_encoding/base/enum.EqualError.html' title='enum data_encoding::base::EqualError'>EqualError</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='trait core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='data_encoding/decode/enum.Error.html' title='enum data_encoding::decode::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
